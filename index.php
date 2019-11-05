7fc5f91629e87cdc32ebec1ba727ab008bbc28a3
Prozis <prozius@tut.by>
2019-11-05 17:52:36 +0300

    footer

--- index.php
+++ index.php
@@ -7,32 +7,28 @@
   </head>
   <body>
   <div class="black">
-    <div class="logo1">MYCOMPANY</div>
-    <div class="menu">
+    <div class="logo1"><h1>MYCOMPANY</h1></div>
+    <div class="menu-main">
       <ul>
-  <li>HOME</li>
-  <li>ABOUT</li>
-  <li>SERVICES</li>
-  <li>PORTFOLIO</li>
-  <li>TESTIMONIALS</li>
-  <li>CONTACT</li>
-  </ul>
+  <li><a href="#" class="current">home</a></li>
+  <li><a href="#">ABOUT</a></li>
+    <li><a href="#">SERVICES</a></li>
+      <li><a href="#">PORTFOLIO</a></li>
+        <li><a href="#">TESTIMONIALS</a></li>
+          <li><a href="#">CONTACT</a></li>
+    </ul>
 
     </div>
 <div class="main-text">
   <h1>WELCOME on mycompany</h1><br>
   We are a digital agency with years of experience and with extraordinary people
 </div>
-<div class="white">
+
+    </div>
+
+  <div class="white">
   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   </div>
 
-<div class="box box3">Box3</div>
-<div class="box box4 light">Box4 </div>
-<div class="box box5">Box5 </div>
-<div class="box box6">Box6 </div>
-
-
-    </div>
   </body>
 </html>
