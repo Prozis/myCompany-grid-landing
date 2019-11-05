145569856ecf74fa9e502d1d88a59779f05dc9df
Prozis <prozius@tut.by>
2019-11-05 17:11:42 +0300

    ad file

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>MyCompany page</title>
+  </head>
+  <body>
+  <div class="black">
+    <div class="logo1">MYCOMPANY</div>
+    <div class="menu">
+      <ul>
+  <li>HOME</li>
+  <li>ABOUT</li>
+  <li>SERVICES</li>
+  <li>PORTFOLIO</li>
+  <li>TESTIMONIALS</li>
+  <li>CONTACT</li>
+  </ul>
+
+    </div>
+<div class="main-text">
+  <h1>WELCOME on mycompany</h1><br>
+  We are a digital agency with years of experience and with extraordinary people
+</div>
+<div class="white">
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </div>
+
+<div class="box box3">Box3</div>
+<div class="box box4 light">Box4 </div>
+<div class="box box5">Box5 </div>
+<div class="box box6">Box6 </div>
+
+
+    </div>
+  </body>
+</html>
