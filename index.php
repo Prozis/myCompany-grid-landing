3288a7aa1535e9952f4133e6fa325abc4b49b6d0
Prozis <prozius@tut.by>
2019-11-06 16:23:47 +0300

    add 3 block

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <title>MyCompany page</title>
   </head>
   <body>
-  <div class="black">
-    <div class="logo1"><h1>MYCOMPANY</h1></div>
+  <div class="black box1">
+    <div class="logo1">MYCOMPANY</div>
     <div class="menu-main">
       <ul>
   <li><a href="#" class="current">home</a></li>
@@ -19,16 +19,32 @@
     </ul>
 
     </div>
-<div class="main-text">
-  <h1>WELCOME on mycompany</h1><br>
+<div class="main-text" center=>
+
+<h1>WELCOME on mycompany</h1><br>
   We are a digital agency with years of experience and with extraordinary people
+
 </div>
 
     </div>
 
-  <div class="white">
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  <div class="white1">
+    <div class="">
+      <img src="img/comp1.png" alt="mackbook">
+    </div>
+    <div class="">
+      <h2>Some words about us</h2>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+<ul>
+  <li>Mission - We deliver uniqueness and quality</li>
+  <li>Skills - Delivering fast and excellent results</li>
+  <li>Clients - Satisfied clients thanks to our experience</li>
+</ul>
+    </div>
   </div>
-
+<div class="black team">
+  <h1>Our team</h1>
+  
+</div>
   </body>
 </html>
