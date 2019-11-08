fe6825a937e5b8ef3dc84c1636dbd89b2ae5de4a
Andrew Proz <prozius@tut.by>
2019-11-08 18:44:46 +0300

    все блоки сверстаны

--- index.php
+++ index.php
@@ -1,50 +1,196 @@
 <!DOCTYPE html>
 <html lang="ru" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/style.css">
-    <title>MyCompany page</title>
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/css/style.css">
+  <title>MyCompany page</title>
+</head>
+<body>
   <div class="black box1">
     <div class="logo1">MYCOMPANY</div>
     <div class="menu-main">
       <ul>
-  <li><a href="#" class="current">home</a></li>
-  <li><a href="#">ABOUT</a></li>
-    <li><a href="#">SERVICES</a></li>
-      <li><a href="#">PORTFOLIO</a></li>
+        <li><a href="#" class="current">home</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">SERVICES</a></li>
+        <li><a href="#">PORTFOLIO</a></li>
         <li><a href="#">TESTIMONIALS</a></li>
-          <li><a href="#">CONTACT</a></li>
-    </ul>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
 
     </div>
-<div class="main-text" center=>
+    <div class="main-text" center=>
 
-<h1>WELCOME on mycompany</h1><br>
-  We are a digital agency with years of experience and with extraordinary people
+      <h1>WELCOME on <span class="yellowtext">mycompany</span></h1><br>
+      We are a digital agency with years of experience and with extraordinary people
 
-</div>
-
-    </div>
-
-  <div class="white1">
-    <div class="">
-      <img src="img/comp1.png" alt="mackbook">
-    </div>
-    <div class="">
-      <h2>Some words about us</h2>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-<ul>
-  <li>Mission - We deliver uniqueness and quality</li>
-  <li>Skills - Delivering fast and excellent results</li>
-  <li>Clients - Satisfied clients thanks to our experience</li>
-</ul>
     </div>
   </div>
-<div class="black team">
-  <h1>Our team</h1>
-  
+
+  <div class="white box2">
+<div class="wrapper" id="box2">
+      <div>
+      <h2><img src="img/comp1.png" alt="mackbook"></h2>
+    </div>
+    <div>
+      <h3><span class="weigth300">Some words </span>about us</h3>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <ul>
+        <li>Mission - We deliver uniqueness and quality</li>
+        <li>Skills - Delivering fast and excellent results</li>
+        <li>Clients - Satisfied clients thanks to our experience</li>
+      </ul>
+    </div>
+  </div>
+
+  </div>
+  <div class="black team">
+    <div class="wrapper">
+    <h2>Our team</h2>
+    <div class="line"></div>
+    <div class="teammen">
+    <div><img src="img/Ellipse 6.png" alt="photo-men"><br>
+      <h4>Vlad Ivanov</h4>
+      Designer<br>
+      Do not seek to change what has come before. Seek to create that which has not.
+    </div>
+    <div><img src="img/Ellipse 6.png" alt="photo-men"><br>
+      <h4>Vlad Ivanov</h4>
+      Designer<br>
+      Do not seek to change what has come before. Seek to create that which has not.
+    </div>
+    <div><img src="img/Ellipse 6.png" alt="photo-men"><br>
+      <h4>Vlad Ivanov</h4>
+      Designer<br>
+      Do not seek to change what has come before. Seek to create that which has not.
+    </div>
+    <div><img src="img/Ellipse 6.png" alt="photo-men"><br>
+      <h4>Vlad Ivanov</h4>
+      Designer<br>
+      Do not seek to change what has come before. Seek to create that which has not.
+    </div>
+  </div>
 </div>
-  </body>
+</div>
+
+<div class="servise white">
+  <div class="wrapper">
+  <h2>our services</h2>
+  <div class="line"></div>
+  <span class="italic">
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ex quidem maiores ullam quis, at asperiores minus ad eaque maxime possimus cum ab iusto assumenda velit excepturi porro ipsa tenetur.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ex quidem maiores ullam quis, at asperiores minus ad eaque maxime possimus cum ab iusto assumenda velit excepturi porro ipsa tenetur.
+</span>
+  <div class="teammen">
+  <div><img src="img/serv1.png" alt="photo-men"><br>
+    <h5>Web design</h5>
+    Designer<br>
+The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+  </div>
+  <div><img src="img/serv2.png" alt="photo-men"><br>
+    <h5>Mobile apps</h5>
+    Designer<br>
+    Do not seek to change what has come before. Seek to create that which has not.
+  </div>
+  <div><img src="img/serv3.png" alt="photo-men"><br>
+    <h5>PHOTOGRAPHY</h5>
+    Designer<br>
+    Do not seek to change what has come before. Seek to create that which has not.
+  </div>
+  <div><img src="img/serv4.png" alt="photo-men"><br>
+    <h5>marketing</h5>
+    Designer<br>
+    Do not seek to change what has come before. Seek to create that which has not.
+  </div>
+  </div>
+</div>
+</div>
+
+<div class="client black">
+  <div class="wrapper">
+  <h2><span class="weigth300">SOME OF </span>OUR CLIENTS</h2>
+  <div class="line"></div>
+  <img src="img/Layer 69.png" alt="chanel">
+  <img src="img/Layer 65.png" alt="chanel">
+  <img src="img/Layer 68.png" alt="chanel">
+  <img src="img/Layer 67.png" alt="chanel">
+  <img src="img/Layer 64.png" alt="chanel">
+</div>
+</div>
+
+<div class="works white">
+<div class="wrapper">
+  <h2><span class="weigth300">take a look at </span>our work</h2>
+  <div class="line"></div>
+  <span class="italic">
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ex quidem maiores ullam quis, at asperiores minus ad eaque maxime possimus cum ab iusto assumenda velit excepturi porro ipsa tenetur.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ex quidem maiores ullam quis, at asperiores minus ad eaque maxime possimus cum ab iusto assumenda velit excepturi porro ipsa tenetur.
+</span>
+  <div id="gallery">
+
+  <div><img src="img/gallery1.png" alt="photo-men"></div>
+  <div><img src="img/gallery1.png" alt="photo-men"></div>
+  <div><img src="img/gallery1.png" alt="photo-men"></div>
+  <div><img src="img/gallery1.png" alt="photo-men"></div>
+  <div><img src="img/gallery1.png" alt="photo-men"></div>
+  <div><img src="img/gallery1.png" alt="photo-men"></div>
+  <div><img src="img/gallery1.png" alt="photo-men"></div>
+  <div><img src="img/gallery1.png" alt="photo-men"></div>
+  </div>
+  </div>
+</div>
+
+<div class="testim black">
+  <div class="wrapper">
+  <h2>our clients’ <span class="weigth300">testimonials</span></h2>
+  <div class="line"></div>
+  <div><h6>This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+  </h6>
+  </div>
+  <div class="">
+    Dean Martin, <span class="weigth300">CEO Acme Inc.</span>
+  </div>
+    </div>
+</div>
+
+<div class="contact white">
+  <div class="wrapper">
+  <h2>contact us</h2>
+  <div class="line"></div>
+  <span class="italic">
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ex quidem maiores ullam quis, at asperiores minus ad eaque maxime possimus cum ab iusto assumenda velit excepturi porro ipsa tenetur.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ex quidem maiores ullam quis, at asperiores minus ad eaque maxime possimus cum ab iusto assumenda velit excepturi porro ipsa tenetur.
+</span>
+
+<form class="form-contact" action="mailform.php" method="post">
+<div class="input-grid">
+  <label for="name" class="form-label">Name<span class="redtext">*</span></label>
+  <input type="text" name="name" value="" class="form-input">
+</div>
+
+<div class="input-grid">
+  <label for="email" class="form-label">Email addres<span class="redtext">*</span></label>
+  <input type="text" name="email" value=""class="form-input">
+</div>
+<div class="area-grid">
+  <label for="message" class="form-label">  Message<span class="redtext">*</span></label>
+    <textarea name="message"  rows="12" class="form-area"></textarea>
+    </div>
+    <div class="button-right">
+      <button type="submit" name="button" class="form-button">SEND</button>
+    </div>
+</form>
+
+</div>
+</div>
+
+<div class="soc black">
+
+
+  <a href="#"><img src="img/facebook.png" alt="soc"></a>
+      <a href="#"><img src="img\twitter.png" alt="soc"></a>
+      <a href="#">  <img src="img\google.png" alt="soc"></a>
+      <a href="#">    <img src="img\linkedin.png" alt="soc"></a>
+        <a href="#">    <img src="img\dribbble.png" alt="soc"></a>
+
+</div>
+
+</body>
 </html>
